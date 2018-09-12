8a540c9e571d63b0087fcee057b8e4de6e95031e
LstChanceMadrid <mmadrid@ceresx.com>
2018-09-12 00:05:43 -0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 const logoSwap = () => {
- let myImage = document.querySelector('img');
- let mySrc = myImage.getAttribute('src');
- if (mySrc === 'images/xbox-logo-1.png') {
-     myImage.setAttribute('src', 'images/xbox-logo-2.png');
- }   else  if (mySrc === 'images/xbox-logo-2.png') {
-     myImage.setAttribute('src', 'images/xbox-logo-3.png');
- } else {
-    myImage.setAttribute('src', 'images/xbox-logo-1.png');
- }
+    let myImage = document.querySelector('img');
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/xbox-logo-1.png') {
+         myImage.setAttribute('src', 'images/xbox-logo-2.png');
+    } else if (mySrc === 'images/xbox-logo-2.png') {
+         myImage.setAttribute('src', 'images/xbox-logo-3.png');
+    } else {
+        myImage.setAttribute('src', 'images/xbox-logo-1.png');
+    }
 }
