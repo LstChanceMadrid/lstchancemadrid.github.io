ec5560618b4af16081fccd7ec9900eb2e0937154
Michael Madrid <Michael.madrid@live.com>
2018-09-12 08:42:02 -0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@ const logoSwap = () => {
     let mySrc = myImage.getAttribute('src');
     if (mySrc === 'images/xbox-logo-1.png') {
          myImage.setAttribute('src', 'images/xbox-logo-2.png');
-    } else if (mySrc === 'images/xbox-logo-2.png') {
-         myImage.setAttribute('src', 'images/xbox-logo-3.png');
+/*  } else if (mySrc === 'images/xbox-logo-2.png') {
+         myImage.setAttribute('src', 'images/xbox-logo-3.png');*/
     } else {
         myImage.setAttribute('src', 'images/xbox-logo-1.png');
     }
